79145d521f2e187b3c0efc9059928059e508552e
Patrick Ewing <patrick.henry.ewing@gmail.com>
2012-12-15 16:18:47 -0800

    Fix POST parsing

--- app.js
+++ app.js
@@ -4,21 +4,22 @@ var app = express();
 var fs = require('fs');
 
 var DroneNotationGenerator = require(__dirname + '/lib/drone-notation/drone-notation');
-var drone  = arDrone.createClient();
+var drone = arDrone.createClient();
 
 app.use(express.static(__dirname + '/web'));
 app.use(express.bodyParser());
 app.set('title', 'DroneML');
 
 app.post('/fly', function(req, res){
-  console.log('Received code:\n', req.body.droneML)
+  console.log('Received DroneML:\n', req.body.droneML);
   var generatedCode = DroneNotationGenerator.generate(req.body.droneML);
   eval(generatedCode);
-  res.send("Running DroneML:\n" + req.body.droneML);
+  console.log('Generated JS:\n', generatedCode);
+  res.send("Running DroneML:\n" + "<pre>" + req.body.droneML + "</pre>");
 });
 
 app.post('/debug', function(req, res){
-  console.log('Received DroneML:\n', req.body.droneML)
+  console.log('Received DroneML:\n', req.body.droneML);
   var generatedCode = DroneNotationGenerator.generate(req.body.droneML);
   console.log('Generated JS:\n', generatedCode);
   res.send("<pre>" + generatedCode + "</pre>");
